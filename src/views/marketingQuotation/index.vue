a9b349d871b165e7d1efb197d00eb7e1182c7bbc
NBSeven <410805964@qq.com>
2022-12-25 20:18:04 +0800

    fix: 补充标签

--- src/views/marketingQuotation/index.vue
+++ src/views/marketingQuotation/index.vue
@@ -1,130 +1,132 @@
 <template>
-  <el-card class="marketingQuotation-page" header="总经理审批" m="2">
-    <div style="margin: 20px 0; float: right" v-if="data.isShowBtn">
-      <el-button class="m-2" type="primary" @click="downLoadSOR">SOR下载</el-button>
-      <el-button class="m-2" type="primary" @click="downLoad3DExploded">3D爆炸图下载</el-button>
-      <el-button class="m-2" type="primary" @click="downTrFile">TR-主方案下载</el-button>
-      <el-button type="primary" @click="jumpToAnalysis">点击查看报价分析看板</el-button>
-      <el-button class="m-2" type="primary" @click="jumpToElec">点击查看电子料返利金额</el-button>
-      <el-button class="m-2" type="primary" @click="jumpToStru">点击查看结构料返利金额</el-button>
-      <el-button class="m-2" @click="toNREPriceList" type="primary">在线预览NRE核价表</el-button>
-      <el-button class="m-2" @click="toProductPriceList" type="primary">在线预览核价表</el-button>
-    </div>
-    <el-descriptions :column="2" border>
-      <el-descriptions-item label="直接客户名称">
-        {{ data.marketingQuotationData.directCustomerName }}
-      </el-descriptions-item>
-      <el-descriptions-item label="客户性质"> {{ data.marketingQuotationData.clientNature }} </el-descriptions-item>
-      <el-descriptions-item label="终端客户名称">
-        {{ data.marketingQuotationData.terminalCustomerName }}
-      </el-descriptions-item>
-      <el-descriptions-item label="终端客户性质">
-        {{ data.marketingQuotationData.terminalClientNature }}
-      </el-descriptions-item>
-      <el-descriptions-item label="开发计划">
-        {{ data.marketingQuotationData.developmentPlan }}
-      </el-descriptions-item>
-      <el-descriptions-item label="汇率"> {{ data.marketingQuotationData.exchangeRate }} </el-descriptions-item>
-    </el-descriptions>
-    <el-card header="走量信息" m="2">
-      <el-table :data="data.marketingQuotationData?.motionMessage" border>
-        <el-table-column type="index" width="100" />
-        <el-table-column label="名称" prop="messageName" />
-        <el-table-column
-          v-for="(item, index) in data?.motionMessageSop"
-          :key="item.year"
-          :label="item.year"
-          :prop="`sop[${index}].value`"
-          :formatter="formatMarketingQuotationDatas"
-        />
-      </el-table>
-    </el-card>
-    <el-card header="核心部件：" m="2">
-      <!-- <div>
+  <div>
+    <el-card class="marketingQuotation-page" header="总经理审批" m="2">
+      <div style="margin: 20px 0; float: right" v-if="data.isShowBtn">
+        <el-button class="m-2" type="primary" @click="downLoadSOR">SOR下载</el-button>
+        <el-button class="m-2" type="primary" @click="downLoad3DExploded">3D爆炸图下载</el-button>
+        <el-button class="m-2" type="primary" @click="downTrFile">TR-主方案下载</el-button>
+        <el-button type="primary" @click="jumpToAnalysis">点击查看报价分析看板</el-button>
+        <el-button class="m-2" type="primary" @click="jumpToElec">点击查看电子料返利金额</el-button>
+        <el-button class="m-2" type="primary" @click="jumpToStru">点击查看结构料返利金额</el-button>
+        <el-button class="m-2" @click="toNREPriceList" type="primary">在线预览NRE核价表</el-button>
+        <el-button class="m-2" @click="toProductPriceList" type="primary">在线预览核价表</el-button>
+      </div>
+      <el-descriptions :column="2" border>
+        <el-descriptions-item label="直接客户名称">
+          {{ data.marketingQuotationData.directCustomerName }}
+        </el-descriptions-item>
+        <el-descriptions-item label="客户性质"> {{ data.marketingQuotationData.clientNature }} </el-descriptions-item>
+        <el-descriptions-item label="终端客户名称">
+          {{ data.marketingQuotationData.terminalCustomerName }}
+        </el-descriptions-item>
+        <el-descriptions-item label="终端客户性质">
+          {{ data.marketingQuotationData.terminalClientNature }}
+        </el-descriptions-item>
+        <el-descriptions-item label="开发计划">
+          {{ data.marketingQuotationData.developmentPlan }}
+        </el-descriptions-item>
+        <el-descriptions-item label="汇率"> {{ data.marketingQuotationData.exchangeRate }} </el-descriptions-item>
+      </el-descriptions>
+      <el-card header="走量信息" m="2">
+        <el-table :data="data.marketingQuotationData?.motionMessage" border>
+          <el-table-column type="index" width="100" />
+          <el-table-column label="名称" prop="messageName" />
+          <el-table-column
+            v-for="(item, index) in data?.motionMessageSop"
+            :key="item.year"
+            :label="item.year"
+            :prop="`sop[${index}].value`"
+            :formatter="formatMarketingQuotationDatas"
+          />
+        </el-table>
+      </el-card>
+      <el-card header="核心部件：" m="2">
+        <!-- <div>
         产品名称：{{ data.marketingQuotationData. }}
       </div> -->
-      <template v-for="item in data.marketingQuotationData.coreComponent" :key="item.messageName">
-        <el-card :header="item.componentName" class="m-2">
-          <el-table :data="item.productSubclass" border>
-            <!-- <el-table-column type="index" width="100" /> -->
-            <el-table-column label="核心部件" prop="coreComponent" />
-            <el-table-column label="型号" prop="model" />
-            <el-table-column label="类型" prop="type" />
-            <el-table-column label="备注" prop="remark" />
-          </el-table>
-        </el-card>
+        <template v-for="item in data.marketingQuotationData.coreComponent" :key="item.messageName">
+          <el-card :header="item.componentName" class="m-2">
+            <el-table :data="item.productSubclass" border>
+              <!-- <el-table-column type="index" width="100" /> -->
+              <el-table-column label="核心部件" prop="coreComponent" />
+              <el-table-column label="型号" prop="model" />
+              <el-table-column label="类型" prop="type" />
+              <el-table-column label="备注" prop="remark" />
+            </el-table>
+          </el-card>
+        </template>
+      </el-card>
+      <el-card header="NRE费用：" m="2">
+        <template v-for="item in data.marketingQuotationData.nreCost" :key="item.messageName">
+          <el-card :header="item.nreCostModuleName" class="m-2">
+            <el-table :data="item.nreCostModels" border>
+              <el-table-column type="index" width="100" />
+              <el-table-column label="费用类别" prop="name" />
+              <el-table-column label="成本" prop="cost" :formatter="formatThousandths" />
+            </el-table>
+          </el-card>
+        </template>
+      </el-card>
+      <el-card header="内部核价信息：" m="2">
+        <template v-for="item in data.marketingQuotationData.pricingMessage" :key="item.messageName">
+          <el-card :header="item.pricingMessageName" class="m-2">
+            <el-table :data="item.pricingMessageModels" border>
+              <el-table-column label="序号" type="index" width="100" />
+              <el-table-column label="费用类别" prop="name" />
+              <el-table-column label="成本值" prop="costValue" :formatter="formatThousandths" />
+            </el-table>
+          </el-card>
+        </template>
+      </el-card>
+      <el-card header="报价策略：" m="2">
+        <el-table :data="data.marketingQuotationData.biddingStrategy" border>
+          <el-table-column type="index" width="100" />
+          <el-table-column label="产品" prop="product" />
+          <el-table-column label="成本" prop="cost" :formatter="formatThousandths" />
+          <el-table-column label="毛利率" prop="grossMargin" :formatter="formatterP" />
+          <el-table-column label="价格" prop="price" :formatter="formatThousandths" />
+          <el-table-column label="佣金" prop="commission" :formatter="formatThousandths" />
+          <el-table-column label="含佣金的毛利率" prop="grossMarginCommission" :formatter="formatterP" />
+        </el-table>
+      </el-card>
+      <el-card header="NRE报价策略：" m="2">
+        <el-table :data="data.marketingQuotationData.expensesStatement" border>
+          <el-table-column type="index" width="100" />
+          <el-table-column label="费用类别" prop="formName" />
+          <el-table-column label="核价金额" prop="pricingMoney" :formatter="formatThousandths" />
+          <el-table-column label="报价系数" prop="offerCoefficient" :formatter="formatter" />
+          <el-table-column label="报价金额" prop="offerMoney" :formatter="formatThousandths" />
+          <el-table-column label="备注" prop="remark" />
+        </el-table>
+      </el-card>
+      <el-descriptions title="" border :column="2">
+        <el-descriptions-item label="核价金额合计">
+          {{ calculationNre("pricingMoney") }}
+        </el-descriptions-item>
+        <el-descriptions-item label="报价金额合计">
+          {{ calculationNre("offerMoney") }}
+        </el-descriptions-item>
+      </el-descriptions>
+      <el-row justify="end" style="margin-top: 20px">
+        <div v-if="data.userInfo.userJobs === '总经理'">
+          <el-button type="primary" @click="handleGeneralManagerQuoteCheck(true)" v-havedone>同意</el-button>
+          <el-button type="danger" @click="handleGeneralManagerQuoteCheck(false)" v-havedone>拒绝</el-button>
+        </div>
+      </el-row>
+    </el-card>
+    <el-dialog v-model="dialogVisible" title="3D爆炸图下载(请选择零件)" width="40%" align-center>
+      <el-space wrap>
+        <div v-for="prop in ProductByAuditFlowId" :key="prop">
+          <el-button type="success" plain @click="downLoad3D(prop.id)">{{ prop.product }}</el-button>
+        </div>
+      </el-space>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="dialogVisible = false">关闭</el-button>
+        </span>
       </template>
-    </el-card>
-    <el-card header="NRE费用：" m="2">
-      <template v-for="item in data.marketingQuotationData.nreCost" :key="item.messageName">
-        <el-card :header="item.nreCostModuleName" class="m-2">
-          <el-table :data="item.nreCostModels" border>
-            <el-table-column type="index" width="100" />
-            <el-table-column label="费用类别" prop="name" />
-            <el-table-column label="成本" prop="cost" :formatter="formatThousandths" />
-          </el-table>
-        </el-card>
-      </template>
-    </el-card>
-    <el-card header="内部核价信息：" m="2">
-      <template v-for="item in data.marketingQuotationData.pricingMessage" :key="item.messageName">
-        <el-card :header="item.pricingMessageName" class="m-2">
-          <el-table :data="item.pricingMessageModels" border>
-            <el-table-column label="序号" type="index" width="100" />
-            <el-table-column label="费用类别" prop="name" />
-            <el-table-column label="成本值" prop="costValue" :formatter="formatThousandths" />
-          </el-table>
-        </el-card>
-      </template>
-    </el-card>
-    <el-card header="报价策略：" m="2">
-      <el-table :data="data.marketingQuotationData.biddingStrategy" border>
-        <el-table-column type="index" width="100" />
-        <el-table-column label="产品" prop="product" />
-        <el-table-column label="成本" prop="cost" :formatter="formatThousandths" />
-        <el-table-column label="毛利率" prop="grossMargin" :formatter="formatterP" />
-        <el-table-column label="价格" prop="price" :formatter="formatThousandths" />
-        <el-table-column label="佣金" prop="commission" :formatter="formatThousandths" />
-        <el-table-column label="含佣金的毛利率" prop="grossMarginCommission" :formatter="formatterP" />
-      </el-table>
-    </el-card>
-    <el-card header="NRE报价策略：" m="2">
-      <el-table :data="data.marketingQuotationData.expensesStatement" border>
-        <el-table-column type="index" width="100" />
-        <el-table-column label="费用类别" prop="formName" />
-        <el-table-column label="核价金额" prop="pricingMoney" :formatter="formatThousandths" />
-        <el-table-column label="报价系数" prop="offerCoefficient" :formatter="formatter" />
-        <el-table-column label="报价金额" prop="offerMoney" :formatter="formatThousandths" />
-        <el-table-column label="备注" prop="remark" />
-      </el-table>
-    </el-card>
-    <el-descriptions title="" border :column="2">
-      <el-descriptions-item label="核价金额合计">
-        {{ calculationNre("pricingMoney") }}
-      </el-descriptions-item>
-      <el-descriptions-item label="报价金额合计">
-        {{ calculationNre("offerMoney") }}
-      </el-descriptions-item>
-    </el-descriptions>
-    <el-row justify="end" style="margin-top: 20px">
-      <div v-if="data.userInfo.userJobs === '总经理'">
-        <el-button type="primary" @click="handleGeneralManagerQuoteCheck(true)" v-havedone>同意</el-button>
-        <el-button type="danger" @click="handleGeneralManagerQuoteCheck(false)" v-havedone>拒绝</el-button>
-      </div>
-    </el-row>
-  </el-card>
-  <el-dialog v-model="dialogVisible" title="3D爆炸图下载(请选择零件)" width="40%" align-center>
-    <el-space wrap>
-      <div v-for="prop in ProductByAuditFlowId" :key="prop">
-        <el-button type="success" plain @click="downLoad3D(prop.id)">{{ prop.product }}</el-button>
-      </div>
-    </el-space>
-    <template #footer>
-      <span class="dialog-footer">
-        <el-button @click="dialogVisible = false">关闭</el-button>
-      </span>
-    </template>
-  </el-dialog>
+    </el-dialog>
+  </div>
 </template>
 
 <script setup lang="ts">
